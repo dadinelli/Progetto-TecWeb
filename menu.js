884e7505c2289e96b99bfbff98e19caaca3b6570
dadinelli <dadi.martinelli@gmail.com>
2025-01-02 20:24:04 +0100

    Aggiornato menu a tendina

--- menu.js
+++ menu.js
@@ -1,5 +1,10 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
     $('.top_menu').click(function(){
         $('.top_menu ul').toggleClass('open');
     });
-});
+});*/
+
+function toggleMenu() {
+    const menu = document.querySelector('.top_menu ul');
+    menu.classList.toggle('open');
+}
