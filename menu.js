c0a9cafcc7b79ae018108520eaae8c11559a3b6d
dadinelli <dadi.martinelli@gmail.com>
2025-01-06 19:19:36 +0100

    Fixato menu a tendina e logo

--- menu.js
+++ menu.js
@@ -1,1 +1,12 @@
+const navToggle = document.getElementById('hamb-button');
+const menuList = document.getElementById('menu-list');
 
+navToggle.addEventListener('click', () => {
+    menuList.classList.toggle('visible');
+});
+
+document.addEventListener('click', (event) => {
+    if (!menuList.contains(event.target) && !navToggle.contains(event.target)) {
+        menuList.classList.remove('visible');
+    }
+});
