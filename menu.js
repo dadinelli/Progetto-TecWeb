4037c8c0232dbb508a048418077747cdcbb91315
dadinelli <dadi.martinelli@gmail.com>
2025-01-06 13:24:28 +0100

    Aggiunti css vario

--- menu.js
+++ menu.js
@@ -1,10 +1,1 @@
-/*$(document).ready(function(){
-    $('.top_menu').click(function(){
-        $('.top_menu ul').toggleClass('open');
-    });
-});*/
 
-function toggleMenu() {
-    const menu = document.querySelector('.top_menu ul');
-    menu.classList.toggle('open');
-}
