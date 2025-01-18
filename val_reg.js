e0389d0086e73a87ed94821921f4259e8cee1037
dadinelli <dadi.martinelli@gmail.com>
2025-01-18 10:04:59 +0100

    corrette funzionalita'

--- val_reg.js
+++ val_reg.js
@@ -1,3 +1,17 @@
+// Gestione del menu a tendina
+const navToggle = document.getElementById('hamb-button');
+const menuList = document.getElementById('menu-list');
+
+navToggle.addEventListener('click', () => {
+    menuList.classList.toggle('visible');
+});
+
+document.addEventListener('click', (event) => {
+    if (!menuList.contains(event.target) && !navToggle.contains(event.target)) {
+        menuList.classList.remove('visible');
+    }
+});
+
 
 //input validation
 function showError(error_id, error_message){
