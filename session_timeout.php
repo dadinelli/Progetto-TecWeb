c2e911675497e83112b4c89513e66dc686c15635
dadinelli <dadi.martinelli@gmail.com>
2025-01-31 11:27:50 +0100

    modifiche

--- session_timeout.php
+++ session_timeout.php
@@ -5,8 +5,8 @@ if(isset($_SESSION['is_logged_in']) && $_SESSION['is_logged_in'] == true){ //con
         // ultima richeista 30 minuti fa
         session_unset();     // array di sessione svuotato
         session_destroy();  // distruzione dati di sessione
-        $DOM = file_get_contents("html/area-riservata.html");
-        echo($DOM);   //se un utente era loggato ma è passato troppo tempo, lo rimando alla pagina di login
+        //$DOM = file_get_contents("html/area-riservata.html");
+        //echo($DOM);   //se un utente era loggato ma è passato troppo tempo, lo rimando alla pagina di login
     }
     $_SESSION['LAST_ACTIVITY'] = time();  //aggiorno il dato temporale dell'ultima attività
 }
